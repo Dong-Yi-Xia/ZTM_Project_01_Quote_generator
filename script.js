2c50c289764c0f1dc3cf86619da74202aa45351a
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-02-11 17:14:12 -0500

    setTimeOut

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//DOM variables 
 const quoteContainer = document.getElementById('quote-container')
 const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
