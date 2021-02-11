646707f4736c73f0ce8b3583c44c0a3f27fb2e04
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-02-11 16:25:26 -0500

    javascript function

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+
+
+
 // Get Quote From API
 async function getQuote() {
-    const proxyUrl = 'https://mighty-cove-69469.herokuapp.com/'
+    const proxyUrl = 'https://proxy-server-aka.herokuapp.com/'
     // API Url/query. Query begins with a ? and using & for more than 1 query
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
         const response = await fetch(proxyUrl + apiUrl);  //response will NOT be set unitl fetch is finish
         const data = await response.json()   //data will NOT be set until response is set in json
-        console.log(data)
+        //If Author is blank, add 'Unknown' 
+        if(data.quoteAuthor === ""){
+            authorText.innerText = "Unknown"
+        }else{
+            authorText.innerText = data.quoteAuthor;
+        }
+        //Reduce font size for long quotes
+        if(data.quoteText.length > 120){
+            quoteText.classList.add('long-quote') //classList.add will add a className 
+        }else{
+            quoteText.classList.remove('long-quote') //classList.remove will remove a className 
+        }
+        quoteText.innerText = data.quoteText;
     } catch(error) {
         getQuote(); //Get another new quote if error 
-        console.log('Whoops, no quote', error);
     }
 }
 
-// On Load
+
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet/?text=${quote} - ${author}`;
+    //open a new window tab
+    window.open(twitterUrl, '_blank')
+}
+
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote) //getQuote callback
+twitterBtn.addEventListener('click', tweetQuote) //tweetQuote callback
+
+
+
+// On Page Load
 getQuote()
 
 
